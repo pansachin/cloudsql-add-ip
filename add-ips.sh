8b0525377bb7178b4730779686a5da7c80cc94f4
Sachin Prasad <sachin.prasad@pantheon.io>
2024-03-07 12:00:01 +0530

    Flag feature

--- add-ips.sh
+++ add-ips.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# Parse flags
+while getopts ":hi:p:" opt; do
+  case $opt in
+    h)
+      echo "Usage: ./add-ips.sh -i <instance> -p <project>"
+      echo "-i - CloudSQL instance name"
+      echo "-p - GCP Project name"
+      echo "-h - Help"
+      exit 1
+    ;;
+    i) instance="$OPTARG"
+    ;;
+    p) project="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+# Validate flags
+if [ -z "$instance" ] || [ -z "$project" ]; then
+  echo "Usage: ./add-ips.sh -i <instance> -p <project>"
+  echo "Run ./add-ips.sh -h for help"
+  exit 1
+fi
+
 # Get Hosted Grafana ip's
 grafana_ips=$(curl -s https://grafana.com/api/hosted-grafana/source-ips.txt)
 echo "Hosted Grafana IP's: $grafana_ips"
@@ -22,4 +48,4 @@ ip=${ip%?}
 echo "IP's: $ip"
 
 # Add ip's to coudsql instance
-gcloud sql instances patch $1 --authorized-networks=$ip --project=$2
+gcloud sql instances patch $instance --authorized-networks=$ip --project=$project
